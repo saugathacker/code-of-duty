9ad7f709059a1abda590f11ea6f1a87fb3038b55
Saugat Shrestha <saugathacker@gmail.com>
2021-02-16 13:15:02 -0600

    Added Shared Prefereneces

--- app/src/main/java/com/example/code_of_duty/location/Location.kt
+++ app/src/main/java/com/example/code_of_duty/location/Location.kt
@@ -2,7 +2,10 @@ package com.example.code_of_duty.location
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Context.LOCATION_SERVICE
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.location.LocationListener
 import android.location.LocationManager
@@ -10,6 +13,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.getSystemService
@@ -33,6 +37,10 @@ class Location : Fragment(), LocationListener {
     private var locationManager : LocationManager? = null
     private var isGPSEnabled = false
 
+    private val SHARED_PREFS:String = "sharedPrefs"
+    private val EDIT_TEXT: String = "editText"
+    private val RADIO_ID: String = "radioId"
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -54,6 +62,21 @@ class Location : Fragment(), LocationListener {
         binding.lifecycleOwner = this
         binding.locationViewModel = viewModel
 
+        retrieveInputData()
+
+        accessLocationPermission()
+
+
+        binding.saveButton.setOnClickListener(){
+            viewModel.onSaveLocation(binding.editTextLocationName.text.toString(),binding.radioGroup.checkedRadioButtonId)
+            binding.editTextLocationName.setText("")
+            binding.radioGroup.check(R.id.indoor)
+        }
+
+        return binding.root
+    }
+
+    private fun accessLocationPermission(){
         if (ContextCompat.checkSelfPermission(requireContext(),
                         Manifest.permission.ACCESS_FINE_LOCATION) !==
                 PackageManager.PERMISSION_GRANTED) {
@@ -73,12 +96,6 @@ class Location : Fragment(), LocationListener {
         } catch (e: Exception) {
             e.printStackTrace()
         }
-
-        binding.saveButton.setOnClickListener(){
-            viewModel.onSaveLocation(binding.editTextLocationName.text.toString(),binding.radioGroup.checkedRadioButtonId)
-        }
-
-        return binding.root
     }
 
 
@@ -86,7 +103,32 @@ class Location : Fragment(), LocationListener {
         viewModel.setCoord(location.latitude,location.longitude)
     }
 
+    override fun onStop() {
+        super.onStop()
+        saveInputData()
+    }
 
+    private fun saveInputData(){
+        val sharedPreferences = requireContext().getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.apply{
+            putString(EDIT_TEXT, binding.editTextLocationName.text.toString())
+            putInt(RADIO_ID, binding.radioGroup.checkedRadioButtonId)
+        }
+        editor.commit()
+    }
+
+    private fun retrieveInputData(){
+        val sharedPreferences = requireContext().getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE)
+        var savedText = sharedPreferences.getString(EDIT_TEXT, null)
+        var savedId  = sharedPreferences.getInt(RADIO_ID, -1)
+
+        binding.editTextLocationName.setText(savedText)
+        when(savedId){
+            R.id.indoor -> binding.indoor.isChecked = true
+            R.id.outdoor -> binding.outdoor.isChecked = true
+        }
+    }
 }
 
 
