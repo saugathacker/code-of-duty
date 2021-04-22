ed2802692e04dd7187e48c82115e415235152631
Saugat Shrestha <saugathacker@gmail.com>
2021-04-22 10:36:48 -0500

    UI improvements on fragment_profile.xml

--- app/src/main/java/com/example/aimsapp/views/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/aimsapp/views/profile/ProfileFragment.kt
@@ -6,9 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.aimsapp.R
 import com.example.aimsapp.databinding.FragmentProfileBinding
+import kotlinx.android.synthetic.main.fragment_profile.view.*
 
 class ProfileFragment : Fragment() {
 
@@ -31,7 +33,19 @@ class ProfileFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        binding.settingsCard.setOnClickListener {
+            Toast.makeText(requireContext(), "Settings has not been implemented", Toast.LENGTH_SHORT).show()
+        }
+        binding.helpCard.setOnClickListener {
+            Toast.makeText(requireContext(), "Help has not been implemented", Toast.LENGTH_SHORT).show()
+        }
+        binding.aboutCard.setOnClickListener {
+            Toast.makeText(requireContext(), "About has not been implemented", Toast.LENGTH_SHORT).show()
+        }
+        binding.logoutCard.setOnClickListener {
+            Toast.makeText(requireContext(), "Logout has not been implemented", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
