664b79470c4e33262485ee891da4654f00964661
adharsh <adharshadhikari333@gmail.com>
2021-04-21 20:19:36 -0500

    Camera

--- app/src/main/java/com/example/aimsapp/views/forms/SourceFormFragment.kt
+++ app/src/main/java/com/example/aimsapp/views/forms/SourceFormFragment.kt
@@ -1,20 +1,29 @@
 package com.example.aimsapp.views.forms
 
+import android.app.Activity
 import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.aimsapp.R
 import com.example.aimsapp.databinding.FragmentSourceFormBinding
+import kotlinx.android.synthetic.main.fragment_source_form.*
 
 class SourceFormFragment: Fragment() {
 
     private lateinit var binding: FragmentSourceFormBinding
     private lateinit var viewModel: FormViewModel
+    private var CAMERA_REQUEST_CODE = 0
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,8 +36,14 @@ class SourceFormFragment: Fragment() {
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
 
-        binding.Submit.setOnClickListener {
+        binding.submitButton.setOnClickListener {
             submitHandler()
+        }
+
+        binding.uploadButton.setOnClickListener {
+            CAMERA_REQUEST_CODE = 200
+            checkPermissionAndOpenCamera(CAMERA_REQUEST_CODE)
+
         }
 
         return binding.root
@@ -41,7 +56,7 @@ class SourceFormFragment: Fragment() {
             alertDialogBuilder.setTitle("Please fill out the form before submitting")
         }
         else{
-            viewModel.setValues(binding.productType.toString(),binding.startDate.toString(), binding.startTime.toString(), binding.endDate.toString().toDouble(),binding.endTime.toString().toDouble(),binding.grossGallons.toString().toDouble(), binding.netGallons.toString().toDouble(), 0.0,0.0, binding.billOfLading.toString())
+//            viewModel.setValues(binding.productCode.toString(),binding.startDate.toString(), binding.startTime.toString(), binding.endDate.toString().toDouble(),binding.endTime.toString().toDouble(),binding.grossGallons.toString().toDouble(), binding.netGallons.toString().toDouble(), 0.0,0.0, binding.billOfLading.toString())
             alertDialogBuilder.setTitle("Form Sent to Dispatcher")
             alertDialogBuilder.setMessage("Product Type: ${viewModel.productType.value} \nStart Date : ${viewModel.startDate.value}  \n" +
                     "Start Time : ${viewModel.startTime.value}\nEnd Date : ${viewModel.endDate.value}  \n" +
@@ -55,11 +70,45 @@ class SourceFormFragment: Fragment() {
     }
 
     private fun formIsEmpty(): Boolean{
-        if (binding.productType.toString() == "" || binding.startDate.toString() == ""
+        if (binding.productCode.toString() == "" || binding.startDate.toString() == ""
             || binding.endDate.toString() == "" || binding.startTime.toString() == ""|| binding.endTime.toString() == ""|| binding.grossGallons.toString() == ""
             || binding.netGallons.toString() == "" || binding.billOfLading.toString() == ""){
             return true
         }
         return false
+    }
+
+    private fun takePhotoFromCamera(requestCode:Int){
+        val intent= Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, requestCode)
+    }
+    private fun checkPermissionAndOpenCamera(requestCode: Int){
+        if (ContextCompat.checkSelfPermission(requireContext(),android.Manifest.permission.CAMERA)
+            ==PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(android.Manifest.permission.CAMERA), 5)
+        }
+        else{
+            takePhotoFromCamera(requestCode)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode==5){
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                takePhotoFromCamera(CAMERA_REQUEST_CODE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == 200 && data != null) {
+            binding.imageView.setImageBitmap(data.extras?.get("data") as Bitmap)
+        }
     }
 }
