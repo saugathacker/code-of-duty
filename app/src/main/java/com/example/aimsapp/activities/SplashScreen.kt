545105f8079efd5a23a512a9a5b4e57cc26ed17a
Saugat Shrestha <saugathacker@gmail.com>
2021-05-03 13:49:51 -0500

    Added login functinality

--- app/src/main/java/com/example/aimsapp/activities/SplashScreen.kt
+++ app/src/main/java/com/example/aimsapp/activities/SplashScreen.kt
@@ -1,6 +1,7 @@
 package com.example.aimsapp.activities
 
 import android.app.ActivityOptions
+import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
@@ -12,30 +13,41 @@ import android.view.animation.AnimationUtils
 import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.aimsapp.R
+import com.example.aimsapp.databinding.SplashScreenBinding
 
 
 class SplashScreen: AppCompatActivity() {
 
     private val SPLASH_TIME_OUT = 1400L
     private var loggedIn = false;
+    private lateinit var binding: SplashScreenBinding
+    private var driverIdList = arrayListOf<String>("D1","D2","D3","CodeOfDuty")
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.splash_screen)
+
         Log.i("MainActivity1", "started splash")
+        binding = DataBindingUtil.inflate(layoutInflater,R.layout.splash_screen,null,false)
+        setContentView(binding.root)
+
 
         val sharedPreferences = this.getSharedPreferences("login shared prefs", Context.MODE_PRIVATE)
 
         loggedIn = sharedPreferences.getBoolean("loggedIn", false)
 
 
-        val truck = findViewById<ImageView>(R.id.truck)
-        val logo = findViewById<ImageView>(R.id.ilogo)
-        val drop = findViewById<ImageView>(R.id.drop)
-        val loginLayout = findViewById<FrameLayout>(R.id.loginLayout)
+        val truck = binding.truck
+        val logo = binding.ilogo
+        val drop = binding.drop
+        val loginLayout = binding.loginLayout
+        val driverId = binding.driverIdEdit
+        val password = binding.passwordEdit
+        val loginButton = binding.loginButton
+
         val dropAnimation = AnimationUtils.loadAnimation(this, R.anim.animation_drop)
         truck.startAnimation(dropAnimation)
         val logoAnimation = AnimationUtils.loadAnimation(this, R.anim.animation_logo)
@@ -57,12 +69,46 @@ class SplashScreen: AppCompatActivity() {
             else{
                 loginLayout.startAnimation(loginAnimation)
                 loginLayout.visibility = View.VISIBLE
+
             }
 
         }, SPLASH_TIME_OUT)
 
+        loginButton.setOnClickListener {
+            val alertDialogBuilder = AlertDialog.Builder(this)
+            if(driverId.text.isNullOrBlank() || password.text.isNullOrBlank()){
+
+                alertDialogBuilder.setTitle("Incorrect Driver Id or Password")
+                alertDialogBuilder.setMessage("Please enter correct information")
+                alertDialogBuilder.setCancelable(false)
+                alertDialogBuilder.setNegativeButton("OK"
+                ){ _, _ ->
+                }
+                val dialog = alertDialogBuilder.create()
+                dialog.show()
+            }
+            else{
+                if (driverIdList.contains(driverId.text.toString())){
+                    sharedPreferences.edit().putBoolean("loggedIn", true)
+                    val i = Intent(this@SplashScreen, MainActivity::class.java)
+                    i.flags = Intent.FLAG_ACTIVITY_REORDER_TO_FRONT
+                    startActivity(i)
+
+                    // close this activity
+                    finish()
+                }
+                else{
+                    alertDialogBuilder.apply {
+                        setTitle("Driver Id does not exist!")
+                        setMessage("Please enter a valid Driver Id")
+                        setCancelable(false)
+                        setNegativeButton("OK"){_,_ ->}
+                    }
+                    val dialog = alertDialogBuilder.create()
+                    dialog.show()
+                }
+            }
+        }
+
     }
-
-
-
 }
