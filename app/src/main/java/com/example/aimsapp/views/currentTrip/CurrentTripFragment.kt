4572566fc786e2456b4a969176e79c0d07017dde
Saugat Shrestha <saugathacker@gmail.com>
2021-05-04 20:15:04 -0500

    Avoided trip details if on break

--- app/src/main/java/com/example/aimsapp/views/currentTrip/CurrentTripFragment.kt
+++ app/src/main/java/com/example/aimsapp/views/currentTrip/CurrentTripFragment.kt
@@ -1,26 +1,22 @@
 package com.example.aimsapp.views.currentTrip
 
 import android.app.Activity
-import android.opengl.Visibility
+import android.app.AlertDialog
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.view.FrameMetrics
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.example.aimsapp.R
-import com.example.aimsapp.database.tripDatabase.Trip
 import com.example.aimsapp.database.tripDatabase.TripDatabase
 import com.example.aimsapp.databinding.FragmentCurrentTripsBinding
-import com.example.aimsapp.views.tripDetailFragment.TripDetailFragment
-import kotlinx.android.synthetic.main.list_item_trip.view.*
 
 
 class CurrentTripFragment : Fragment() {
@@ -28,6 +24,10 @@ class CurrentTripFragment : Fragment() {
     private lateinit var binding: FragmentCurrentTripsBinding
     private lateinit var viewModel: CurrentTripViewModel
     private lateinit var myContext: FragmentActivity
+    private lateinit var adapter: TripAdapter
+
+    private lateinit var sharedPreferences: SharedPreferences
+    private var onBreak = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,8 +45,26 @@ class CurrentTripFragment : Fragment() {
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
 
-        val adapter = TripAdapter(TripListener {
+        sharedPreferences = requireActivity().getSharedPreferences("tripStatus shared prefs", Context.MODE_PRIVATE)
+        onBreak = sharedPreferences.getBoolean("break", false)
+
+        adapter = TripAdapter(TripListener {
             this.findNavController().navigate(CurrentTripFragmentDirections.actionCurrentTripToTripDetailFragment(it))
+        }, onBreak, TripListener{
+            val alertDialogBuilder = AlertDialog.Builder(requireContext())
+            alertDialogBuilder.apply {
+                setTitle("It looks like you are on a break.")
+                setMessage("Go to Profile page to end the break.")
+                setNegativeButton("Ok"){_,_ ->
+                }
+                setPositiveButton("Take me there"){_,_->
+                   val frag = parentFragment
+                    if (frag != null) {
+                        frag.findNavController().navigate(CurrentTripFragmentDirections.actionCurrentTripToProfile())
+                    }
+                }
+            }
+            alertDialogBuilder.create().show()
         })
 
         binding.tripRecyclerView.adapter = adapter
@@ -70,6 +88,8 @@ class CurrentTripFragment : Fragment() {
 
         return binding.root
     }
+
+
 
     override fun onAttach(activity: Activity) {
         myContext = activity as FragmentActivity
