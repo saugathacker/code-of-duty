cc6b7644dea7615af73bce8dad468e60782ec3e5
emeraldsingh <singhe@warhawks.ulm.edu>
2021-05-05 23:06:43 -0500

    Documentation of ProfileViewModelFactory.kt

--- app/src/main/java/com/example/aimsapp/views/profile/ProfileViewModelFactory.kt
+++ app/src/main/java/com/example/aimsapp/views/profile/ProfileViewModelFactory.kt
@@ -4,10 +4,16 @@ import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
+/**
+ * Constructor for Profile View Model
+ */
 class ProfileViewModelFactory(private val application: Application):
     ViewModelProvider.Factory {
     @Suppress("unchecked_cast")
 
+    /**
+     * please refer to android sdk function for this overridden method
+     */
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if(modelClass.isAssignableFrom(ProfileViewModel::class.java)){
             return ProfileViewModel(application) as T
