ed9eedc52850c7739290299b28241ebfad356379
emeraldsingh <singhe@warhawks.ulm.edu>
2021-05-05 21:08:14 -0500

    Documentation of TripRepository.kt

--- app/src/main/java/com/example/aimsapp/repository/TripRepository.kt
+++ app/src/main/java/com/example/aimsapp/repository/TripRepository.kt
@@ -10,19 +10,26 @@ import com.example.aimsapp.network.Trips
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
+/**
+ * Class that connects the app with network and database
+ */
 class TripRepository(private val database: TripDatabase) {
 
-    fun getTrips():LiveData<List<Trip>> = database.dao.getAllTrips()
-    fun getWaPointById(tripId: Long):LiveData<List<WayPoint>> = database.dao.getWayPointsByTripId(tripId)
+    fun getTrips(): LiveData<List<Trip>> = database.dao.getAllTrips()
+    fun getWaPointById(tripId: Long): LiveData<List<WayPoint>> =
+        database.dao.getWayPointsByTripId(tripId)
 
     suspend fun updateTrip(trip: Trip) = database.dao.updateTrip(trip)
     suspend fun updatePoint(point: WayPoint) = database.dao.updatePoint(point)
-    suspend fun getFormById(seqNum: Long, tripId: Long) = database.dao.getFormById(seqNum,tripId)
+    suspend fun getFormById(seqNum: Long, tripId: Long) = database.dao.getFormById(seqNum, tripId)
     suspend fun insertForm(form: Form) = database.dao.insertForm(form)
     suspend fun updateForm(form: Form) = database.dao.updateForm(form)
-    suspend fun getWayPointByIds(tripId: Long, seqNum: Long) = database.dao.getWayPointByIds(tripId,seqNum)
+    suspend fun getWayPointByIds(tripId: Long, seqNum: Long) =
+        database.dao.getWayPointByIds(tripId, seqNum)
 
-
+    /**
+     * Refreshes trips and waypoints in app
+     */
     suspend fun refreshTrips() {
         withContext(Dispatchers.IO) {
             val response = TripApi.retrofitService.getProperties("codeofduty").await()
