52f4b764c0c77916782c4fe2f512e233644dc121
emeraldsingh <singhe@warhawks.ulm.edu>
2021-05-05 21:02:14 -0500

    Documentation of TripApiService.kt

--- app/src/main/java/com/example/aimsapp/network/TripApiService.kt
+++ app/src/main/java/com/example/aimsapp/network/TripApiService.kt
@@ -13,16 +13,25 @@ private const val BASE_URL = "https://api.appery.io/rest/1/apiexpress/api/Dispat
 private const val DRIVER_ID = "CodeOfDuty"
 private const val API_KEY = "f20f8b25-b149-481c-9d2c-41aeb76246ef"
 
+/**
+ * Converts Json adapter to data
+ */
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
     .build()
 
+/**
+ * Method that pulls from the API
+ */
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
     .addCallAdapterFactory(CoroutineCallAdapterFactory())
     .baseUrl(BASE_URL)
     .build()
 
+/**
+ * interface that adds api calls Driver_ID and API_KEy
+ */
 interface TripApiService {
     @GET("GetDetailedTripListByDriver/{driverId}?apiKey=$API_KEY")
     fun getProperties(
@@ -31,6 +40,10 @@ interface TripApiService {
             Deferred<Response>
 }
 
+/**
+ * instantiating retrofitService
+ * lazy initialization is the tactic of delaying the creation of an object
+ */
 object TripApi{
     val retrofitService: TripApiService by lazy {
         retrofit.create(TripApiService::class.java)
